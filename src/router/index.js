1ab7e5d9e9a24fb3438b6bbce15c8571f71d19bb
FakeStandard <rileylin.space@gmail.com>
2022-02-10 14:36:13 +0800

    fixed

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+// import { createRouter, createWebHashHistory } from 'vue-router'
+// import Home from '../views/Home.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
-]
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: Home
+//   }
+// ]
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+// const router = createRouter({
+//   history: createWebHashHistory(),
+//   routes
+// })
 
-export default router
+// export default router
